ab995ab02c3dd17e6c10e5280d7f3f790f3abe18
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-28 12:26:32 +0530

    Commit

---
+++ src/main/java/com/example/SpringSecurityP02/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.example.SpringSecurityP02.config;
+
+import com.example.SpringSecurityP02.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebConfig {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
+
+        AuthenticationManagerBuilder builder=http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        builder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+
+        return builder.build();
+    }
+}
