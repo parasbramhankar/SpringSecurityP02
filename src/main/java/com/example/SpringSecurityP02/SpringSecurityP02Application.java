bfc20bc26a4919bb45f9807b869d42a43a463e2c
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-28 09:38:08 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringSecurityP02/SpringSecurityP02Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityP02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityP02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityP02Application.class, args);
+	}
+
+}
