d96857ea22063e16d235e2021ab4b74fbd6dff3d
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-28 13:40:36 +0530

    Commit

--- src/main/java/com/example/SpringSecurityP02/controller/TestController.java
+++ src/main/java/com/example/SpringSecurityP02/controller/TestController.java
@@ -10,46 +10,44 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/")
-    @PreAuthorize("hashRole('ADMIN')")
-    public String test(){
+    @PreAuthorize("hasRole('ADMIN')")
+    public String test() {
         return "this is test class";
     }
 
-
     @GetMapping("/contactUs")
-    @PreAuthorize("hashRole('USER')")
-    public String contactUs(){
+    @PreAuthorize("hasRole('USER')")
+    public String contactUs() {
         return "This is the contact us page";
     }
 
     @GetMapping("/aboutUs")
-    @PreAuthorize("hashRole('USER')")
-    public String aboutUs(){
-        return "This is the about us page ";
+    @PreAuthorize("hasRole('USER')")
+    public String aboutUs() {
+        return "This is the about us page";
     }
 
     @GetMapping("/updates")
-    @PreAuthorize("hashRole('USER')")
-    public String updates(){
+    @PreAuthorize("hasRole('USER')")
+    public String updates() {
         return "This is the update page";
     }
 
     @GetMapping("/transfer")
-    @PreAuthorize("hashRole('USER')")
-    public String transfer(){
+    @PreAuthorize("hasRole('USER')")
+    public String transfer() {
         return "This is the transfer page";
     }
 
     @GetMapping("/transactions")
-    @PreAuthorize("hashRole('USER') or hashRole('ADMIN')")
-    public String printAllTransactions(){
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public String printAllTransactions() {
         return "printing transactions...";
     }
 
     @GetMapping("/getBalance")
-    @PreAuthorize("hashRole('USER') or hashRole('ADMIN')")
-    public String getBalance(){
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public String getBalance() {
         return "This is the getBalance page";
     }
-
 }
